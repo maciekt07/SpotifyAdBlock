ff5de0980841d0e29a153e96d056f6c77d71ba05
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-11-01 21:14:40 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace SpotifyNowPlaying
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("SpotifyNowPlaying Author: github.com/maciekkoks");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("enter the delay in ms: ");
+            int delay = Int32.Parse(Console.ReadLine());
+            Console.WriteLine();
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                var proc = Process.GetProcessesByName("Spotify").LastOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
+                if (proc == null)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Spotify is not running");
+                }
+                if (proc != null)
+                {
+                    Console.WriteLine(proc.MainWindowTitle);
+                }
+                Thread.Sleep(delay);
+               
+            }
+        }
+
+    }
+}
